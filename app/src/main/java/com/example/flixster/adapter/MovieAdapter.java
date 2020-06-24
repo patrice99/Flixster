11be120f22f3d139061fb2803e6d6b060b224281
Patrice Gill <pgill30@fb.com>
2020-06-24 17:55:56 -0400

    Updated Movie Adapter

--- app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +31,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
         View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
         return new ViewHolder(movieView);
     }
@@ -38,6 +40,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     //Involves populating data into the item through holder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
         //Get the movie at the passed position
         Movie movie = movies.get(position);
 
