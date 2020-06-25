b407e9ec32f5d66bc74e16b8ff6d9566cb8a0f87
Patrice Gill <pgill30@fb.com>
2020-06-25 19:28:46 -0400

    Added picture to Movie Details Activity

--- app/src/main/java/com/example/flixster/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/activities/MovieDetailsActivity.java
@@ -2,16 +2,22 @@ package com.example.flixster.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
 import org.parceler.Parcels;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
@@ -23,6 +29,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
     TextView tvOverView;
     RatingBar rbVoteAverage;
     TextView tvPopularity;
+    ImageView ivPoster;
 
 
 
@@ -40,6 +47,7 @@ public class MovieDetailsActivity extends AppCompatActivity {
         tvOverView = (TextView) findViewById(R.id.tvOverview);
         rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
         tvPopularity = (TextView) findViewById(R.id.tvPopularity);
+        ivPoster = (ImageView) findViewById(R.id.ivPoster);
 
         //set the text for title and overview
         tvTitle.setText(movie.getTitle());
@@ -49,6 +57,26 @@ public class MovieDetailsActivity extends AppCompatActivity {
         //set the Rating bar
         float voteavg = movie.getVoteAverage().floatValue();
         rbVoteAverage.setRating(voteavg > 0 ? voteavg / 2.0f : voteavg);
+
+        //set the imageView
+        String imgUrl;
+        int plcholder;
+        //if phone is in landscape
+        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            //imgUrl = backdropPath
+            imgUrl = movie.getBackDropPath();
+            plcholder = R.drawable.flicks_movie_placeholder;
+
+        } else {
+            //imgUrl = poster path
+            imgUrl = movie.getPosterPath();
+            plcholder = R.drawable.flicks_backdrop_placeholder;
+        }
+
+        int radius = 30; //corner radius
+        int margin = 10; //crop margin
+        Glide.with(this).load(imgUrl).placeholder(plcholder).transform(new RoundedCornersTransformation(radius, margin)).into(ivPoster);
+
 
     }
 }
