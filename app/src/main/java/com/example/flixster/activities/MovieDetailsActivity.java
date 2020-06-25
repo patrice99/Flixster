9dfc5e04045c878b526580a41a394fc6c8d5d022
Patrice Gill <pgill30@fb.com>
2020-06-25 16:40:47 -0400

    Update MovieDetailsActivity

--- app/src/main/java/com/example/flixster/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixster/activities/MovieDetailsActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
@@ -13,7 +15,13 @@ import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
+    //the movie details we want to display
     Movie movie;
+
+    //the view Objects
+    TextView tvTitle;
+    TextView tvOverView;
+    RatingBar rbVoteAverage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,19 @@ public class MovieDetailsActivity extends AppCompatActivity {
         //unwrap the movie passed via intent, using its simple name as a key
         movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
         Log.d("MovieDetailsActivity", "Showing details for " + movie.getTitle());
+
+        //resolve the view objects
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverView = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        //set the text for title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverView.setText(movie.getOverview());
+
+        //set the Rating bar
+        float voteavg = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteavg > 0 ? voteavg / 2.0f : voteavg);
+
     }
 }
