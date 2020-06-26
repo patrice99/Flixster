a0090c4b07a23e106df6ed060621355525803cdc
Patrice Gill <pgill30@fb.com>
2020-06-25 21:46:40 -0400

    Change toolbar color

--- app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
